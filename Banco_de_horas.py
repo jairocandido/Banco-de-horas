6a494717029bf692ab4e94cd1d95d3163ec8a43c
jairocandido <84736469+jairocandido@users.noreply.github.com>
2023-11-10 14:28:37 -0300

    Banco de horas commit

---
+++ Banco_de_horas.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pandas as pd
+
+# Configurar a página
+st.set_page_config(
+    page_title="Banco de Horas",
+    page_icon=":hourglass:",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Função para formatar timedelta
+def format_timedelta(timedelta):
+    total_seconds = int(timedelta.total_seconds())
+    hours, remainder = divmod(abs(total_seconds), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{'-' if total_seconds < 0 else ''}{hours:02}:{minutes:02}:{seconds:02}"
+
+# Carregar os dados
+file_path = r'C:\Python\Banco_de_horas\Recursos\Extensão de jornada x compensação.xlsx'
+extensao_jornada = pd.read_excel(file_path, sheet_name="EXTENSÃO DE JORNADA")
+horas_compensadas = pd.read_excel(file_path, sheet_name="HORAS COMPENSADAS")
+
+# Renomear a coluna 'Cod_Guarda' para 'Servidor'
+extensao_jornada = extensao_jornada.rename(columns={'Cod_Guarda': 'Servidor'})
+
+# Tratar dados faltantes
+extensao_jornada["Horas/minutos extras"] = extensao_jornada["Horas/minutos extras"].fillna("0:00:00")
+horas_compensadas["Horas a serem compensadas"] = horas_compensadas["Horas a serem compensadas"].fillna("0:00:00")
+extensao_jornada["Servidor"] = extensao_jornada["Servidor"].fillna("Dados Faltantes")
+horas_compensadas["Servidor"] = horas_compensadas["Servidor"].fillna("Dados Faltantes")
+
+# Converter as colunas de tempo para o formato de tempo
+extensao_jornada["Horas/minutos extras"] = pd.to_timedelta(extensao_jornada["Horas/minutos extras"])
+horas_compensadas["Horas a serem compensadas"] = pd.to_timedelta(horas_compensadas["Horas a serem compensadas"])
+
+# Calcular o saldo do banco de horas
+total_horas_extras = extensao_jornada.groupby("Servidor")["Horas/minutos extras"].sum()
+total_horas_compensadas = horas_compensadas.groupby("Servidor")["Horas a serem compensadas"].sum()
+banco_de_horas = pd.DataFrame({
+    "Total Horas Extras": total_horas_extras,
+    "Total Horas Compensadas": total_horas_compensadas
+})
+banco_de_horas = banco_de_horas.fillna(pd.to_timedelta("0:00:00"))
+banco_de_horas["Saldo Banco de Horas"] = banco_de_horas.apply(
+    lambda row: row["Total Horas Extras"] - row["Total Horas Compensadas"]
+    if row["Total Horas Extras"] >= row["Total Horas Compensadas"]
+    else pd.to_timedelta((row["Total Horas Extras"] - row["Total Horas Compensadas"]).total_seconds(), unit='s'),
+    axis=1
+)
+
+# Formatar a exibição do saldo do banco de horas
+banco_de_horas["Saldo Banco de Horas"] = banco_de_horas["Saldo Banco de Horas"].apply(format_timedelta)
+
+# Estilos CSS personalizados
+st.markdown("""
+    <style>
+        .css-2trqyj {
+            width: 50% !important;
+        }
+        .css-1d391kg {
+            padding: 0em 0em !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Título e Subtítulo
+st.title('Sistema de Banco de Horas')
+st.subheader('Consulte o saldo do banco de horas e as datas de compensação')
+
+# Layout com containers
+with st.container():
+    col1, col2 = st.columns([1, 2])
+    
+    # Selecionar servidor
+    with col1:
+        servidor_selecionado = st.selectbox('Selecione um servidor:', banco_de_horas.index.unique())
+    
+    # Mostrar saldo do banco de horas
+    with col2:
+        saldo = banco_de_horas.loc[servidor_selecionado, "Saldo Banco de Horas"]
+        st.metric("Saldo Banco de Horas", saldo)
+
+# Mostrar datas e horas de compensação
+with st.container():
+    compensacoes = horas_compensadas[horas_compensadas["Servidor"] == servidor_selecionado][["Data_compensacao", "Horas a serem compensadas"]]
+    compensacoes = compensacoes.dropna(subset=["Data_compensacao"])
+    if compensacoes.empty:
+        st.info('Este servidor não possui horas compensadas.')
+    else:
+        compensacoes = compensacoes.rename(columns={"Data_compensacao": "Data de compensação", "Horas a serem compensadas": "Horas utilizadas"})
+        compensacoes["Data de compensação"] = compensacoes["Data de compensação"].dt.strftime('%Y-%m-%d')
+        compensacoes["Horas utilizadas"] = compensacoes["Horas utilizadas"].apply(format_timedelta)
+        st.table(compensacoes)
